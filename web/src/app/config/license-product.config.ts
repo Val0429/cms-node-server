2eea3aec85217477cb064ba40405421f53db5c77
Mohammad Firdaus Ardiansyah <mfirdaus2007@gmail.com>
2018-11-14 11:32:26 +0800

    Add feature 9302 License registration for failover products

--- web/src/app/config/license-product.config.ts
+++ web/src/app/config/license-product.config.ts
@@ -1,15 +1,15 @@
 export const LicenseProduct = [
     {
         No: '00166',
-        Type: 'Device',
+        Type: 'iSAP NVR',
         Description: ['CMS 3, support iSAP NVR, count by camera']
     }, {
         No: '00167',
-        Type: '3rd NVR',
+        Type: '3rd party NVR',
         Description: ['CMS 3, support 3rd party NVR, count by camera']
     }, {
         No: '00168',
-        Type: 'Backend Record',
+        Type: 'Backend Recorder',
         Description: ['CMS 3, support Backend Recording, count by camera']
     }, {
         No: '00169',
@@ -24,6 +24,30 @@ export const LicenseProduct = [
         Type: 'IP Camera',
         Description: ['CMS 3, support connecting camera directly, count by camera']
     }, {
+        No: '00176',
+        Type: 'iSAP NVR Failover',
+        Description: ['CMS 3 Failover for iSAP NVR, count by camera']
+    }, {
+        No: '00177',
+        Type: '3rd Party NVR Failover',
+        Description: ['CMS 3 Failover for 3rd party NVR, count by camera']
+    }, {
+        No: '00178',
+        Type: 'Backend Recorder Failover',
+        Description: ['CMS 3 Failover for Backend Recording, count by server']
+    }, {
+        No: '00179',
+        Type: 'Smart Patrol Failover',
+        Description: ['CMS 3 Failover for Smart Patrol, count by mobile device']
+    }, {
+        No: '00180',
+        Type: 'Smart Monitor Failover',
+        Description: ['CMS 3 Failover for Smart Monitor, count by terminal']
+    },  {
+        No: '00181',
+        Type: 'IP Camera Failover',
+        Description: ['CMS 3 Failover for connecting camera directly, count by camera']
+    },{
         No: '99999',
         Type: 'Unregistered Type',
         Description: ['This license type is not suitable for CMS 3.']
