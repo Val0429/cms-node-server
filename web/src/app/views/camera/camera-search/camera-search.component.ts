2deb9fa32c0cf1ffc9ce86879819f4773425e99b
Mohammad Firdaus Ardiansyah <mfirdaus2007@gmail.com>
2018-11-27 13:22:57 +0800

    Add feature 9299 multiple vendor search

--- web/src/app/views/camera/camera-search/camera-search.component.ts
+++ web/src/app/views/camera/camera-search/camera-search.component.ts
@@ -3,6 +3,7 @@ import { CoreService } from 'app/service/core.service';
 import { DeviceVendor } from 'app/config/device-vendor.config';
 import { ArrayHelper } from 'app/helper/array.helper';
 import { ISearchCamera } from 'lib/domain/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-camera-search',
@@ -14,67 +15,61 @@ export class CameraSearchComponent implements OnInit {
   @Output() closeModal: EventEmitter<any> = new EventEmitter();
   /** 搜尋結果 */
   searchList: ISearchCamera[];
-  /** 搜尋廠牌 */
-  searchVendor: string;
   /** Flag:表示loading */
   /** 廠牌選單 */
-  deviceVendors: string[];
+  deviceVendors= DeviceVendor;
   flag = {
     load: false
   };
 
   constructor(private coreService: CoreService) { }
 
-  ngOnInit() {
-    this.deviceVendors = DeviceVendor.filter(x => x.Name.toLowerCase() !== 'customization').map(x => x.Name);
-    this.searchVendor = this.deviceVendors[0];
+  ngOnInit() {    
+    
   }
 
-  clickSearch() {
-    if (!this.searchVendor) {
+  async clickSearch() {
+    if (this.selectedVendors.length==0) {
       alert('Please select one Manufacturer.');
       return;
     }
 
-    this.flag.load = true;
-    this.searchList = undefined;
-
-    const search$ = this.coreService.proxyMediaServer({
-      method: 'GET',
-      path: this.coreService.urls.URL_MEDIA_SEARCH_CAMERA + '?vendor=' + this.searchVendor
-    }, 60000)
-      .map(result => {
-        this.searchList = ArrayHelper.toArray((result && result.Camera.DATA) ? result.Camera.DATA : []);
-        if (!this.searchList) {
-          this.searchList = [];
-        }
-      });
-
-    search$
-      .toPromise()
-      .catch(alert)
-      .then(() => this.flag.load = false);
+    try{      
+      this.searchList = [];
+      this.flag.load = true;
+      for(let vendor of this.selectedVendors){
+        //push observerable item
+        const search$ = this.coreService.proxyMediaServer({
+          method: 'GET',
+          path: this.coreService.urls.URL_MEDIA_SEARCH_CAMERA + '?vendor=' + vendor.toLowerCase()
+        }, 60000)
+          .map(result => {
+            let resultArray = ArrayHelper.toArray((result && result.Camera.DATA) ? result.Camera.DATA : []);
+            this.searchList.push(...resultArray);
+          });
+        await search$.toPromise();
+      }
+    }catch(err){
+      alert(err);
+    }finally{
+      this.flag.load = false;
+    }
   }
-
-  setVendor($event: any) {
-    this.searchVendor = this.getVendorParam($event.target.value);
-    this.searchList = undefined;
+  selectedVendors:string[]=[];
+  setVendors(checked: boolean, vendor:string) {
+    if(checked===true) this.selectedVendors.push(vendor);
+    else this.selectedVendors.splice(this.selectedVendors.findIndex(x=>x == vendor), 1);
+    
+    console.debug("this.selectedVendors", checked, this.selectedVendors);
   }
 
   getVendorParam(vendor: string) {
     const lower = vendor.toLowerCase();
-    switch (lower) {
-      case 'a-mtk':
-      case 'd-link': return lower.replace('-', '');
-      case 'ip surveillance': return lower.replace(' ', '');
-      case 'xts corp.': return 'xts';
-      default: return lower;
-    }
+    return lower;    
   }
 
   clickAddCamera(camera: ISearchCamera) {
     console.debug("clickAddCamera", camera);
-    //camera.COMPANY = this.searchVendor; // 為了讓brand紀錄的大小寫完全符合，直接從選單複製
     this.addDevice.emit(camera);
     this.closeModal.emit();
   }
