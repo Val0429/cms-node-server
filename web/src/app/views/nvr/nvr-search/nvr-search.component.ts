865f132f5f9fddefaf5e28fe964dd8efdc1f51a9
Mohammad Firdaus Ardiansyah <mfirdaus2007@gmail.com>
2018-11-27 13:37:01 +0800

    Add feature 9300 multiple NVR vendor search

--- web/src/app/views/nvr/nvr-search/nvr-search.component.ts
+++ web/src/app/views/nvr/nvr-search/nvr-search.component.ts
@@ -13,7 +13,7 @@ import ArrayHelper from 'app/helper/array.helper';
 export class NvrSearchComponent implements OnInit {
   @Output() addNVR: EventEmitter<any> = new EventEmitter();
   searchList: any;
-  searchVendor: string;
+  
   vendorOptions = NvrManufacturer.SearchList;
   jsonHelper = JsonHelper.instance;
   flag = {
@@ -22,43 +22,52 @@ export class NvrSearchComponent implements OnInit {
   constructor(private coreService: CoreService) { }
 
   ngOnInit() {
-    this.searchVendor = this.vendorOptions[0];
+    
   }
 
-  clickSearch() {
-    if (!this.searchVendor) {
+  async clickSearch() {
+    if (this.selectedVendors.length==0) {
       alert('Please select one Manufacturer.');
       return;
     }
+    try{
     this.flag.load = true;
-    this.searchList = undefined;
-
+    this.searchList = [];
+      for(let vendor of this.selectedVendors){
     const search$ = this.coreService.proxyMediaServer({
       method: 'GET',
-      path: this.coreService.urls.URL_MEDIA_SEARCH_DEVICE + '?vendor=' + this.searchVendor
+      path: this.coreService.urls.URL_MEDIA_SEARCH_DEVICE + '?vendor=' + vendor
     }, 30000)
       .map(result => {
-        this.searchList = ArrayHelper.toArray(this.jsonHelper.findAttributeByString(result, 'AllNVR.NVR'));
-        if (!this.searchList) {
-          this.searchList = [];
-        }
+        if(!result)return;
+        
+        console.debug("nvr search result", result);
+        let allNvr = this.jsonHelper.findAttributeByString(result, 'AllNVR.NVR');
+        let resultArray = ArrayHelper.toArray( allNvr ? allNvr : []);
+        this.searchList.push(...resultArray);
       });
 
-    search$
-      .toPromise()
-      .catch(alert)
-      .then(() => this.flag.load = false);
+      await search$.toPromise();
+      }
+    }catch(error){
+      alert(error);
+    }
+      finally{ this.flag.load = false;
+      }
   }
 
-  setVendor($event: any) {
-    const vendor = $event.target.value;
-    this.searchVendor = vendor;
-    this.searchList = undefined;
+  selectedVendors:string[]=[];
+  setVendors(checked: boolean, vendor:string) {
+    if(checked===true) this.selectedVendors.push(vendor);
+    else this.selectedVendors.splice(this.selectedVendors.findIndex(x=>x == vendor), 1);
+    
+    console.debug("this.selectedVendors", checked, this.selectedVendors);
   }
 
   // 將搜尋到的NVR回傳給父組件新增
   clickAddNVR(nvr: any) {
-    nvr.$.Driver = this.searchVendor;
+    console.debug("nvr", nvr);
+    //nvr.$.Driver = this.searchVendor;
     this.addNVR.emit({ nvr: nvr });
   }
 }
