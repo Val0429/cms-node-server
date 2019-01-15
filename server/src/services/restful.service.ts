96927a7980cdb7db2c4082e6334b4ae0876a3be8
Mohammad Firdaus Ardiansyah <mfirdaus2007@gmail.com>
2019-01-15 11:41:29 +0800

    Format json similar to parse object

--- server/src/services/restful.service.ts
+++ server/src/services/restful.service.ts
@@ -22,7 +22,9 @@ export class RestFulService {
             let page = parseInt(req.query["page"] || "1");                            
             let pageSize = parseInt(req.query["pageSize"] || "50");                                
             let className = req.params["className"];
-            let where = JSON.parse(req.query["where"] || "{}");
+            //mask fieldname to follow parse format
+            let where = JSON.parse((req.query["where"] || "{}"));
+            this.sanitizeQuery(where);
             let include = req.query["include"];
             let data = [];
             let total = 0;
@@ -30,7 +32,7 @@ export class RestFulService {
             if(pageSize>1000)pageSize=1000;
             else if(pageSize<1)pageSize=1;
             if(page<1)page=1;
-
+            
             await Promise.all([
                 this.getData(className, page, pageSize, where, include).then(res=>data =res),
                 this.getCount(className, where).then(res=>total=res)
@@ -54,10 +56,15 @@ export class RestFulService {
             let className = req.params["className"];
             let data = req.body;
             for(let item of data){
+                item._id= this.mongoist.ObjectId().toString();                                
+                this.preProcessJson(item);
                 item._created_at=new Date();
                 item._updated_at=new Date();
             }
             let result = await this.db.collection(className).insertMany(data);
+            for(let item of result){
+                this.postProcessJson(item);
+            }
             res.json(result);
         }
         catch(err){
@@ -86,7 +93,9 @@ export class RestFulService {
         return this.jsonPointer.get(obj, "/"+fieldNames.join("/"));
     }
     updateTargetField(obj:any,fieldNames:string[],val:any){        
-        this.jsonPointer.set(obj, "/"+fieldNames.join("/"), val);
+        let newPath="/"+fieldNames.join("/");
+        //console.log(newPath, val);
+        this.jsonPointer.set(obj, newPath, val);
     }
     async fetchInclude(includeRequest:string, data:any[]){
         let includes = this.constructInclude(includeRequest);
@@ -99,17 +108,14 @@ export class RestFulService {
                 for(let record of data){      
                     var target = this.getTargetField(record, include.fieldNames);
                     //console.log(target);
-                    if(!target)continue;
-                    //change from className$objectId to parent json object
-                    let link = target.split('$');
-                    //console.log(link);
-                    if(link.length<2)continue;
-                    
-                    let parentClassName = link[0];
-                    let _id = link[1];
-                    const getData$ = this.db.collection(parentClassName).find({_id}).then(res=>{ 
-                        //console.log(res);                        
-                        if(res&&res.length>0) this.updateTargetField(record, include.fieldNames, res[0]);                           
+                    const getData$ = this.db.collection(target.className).find({_id:target.objectId}).then(res=>{ 
+                        //console.log(res);
+                        if(res&&res.length>0) {
+                            res[0].__type="Object";
+                            res[0].className=target.className;
+                            this.postProcessJson(res[0]);
+                            this.updateTargetField(record, include.fieldNames, res[0]);                           
+                        }
                     });
                     promises.push(getData$);                    
                 }
@@ -118,10 +124,67 @@ export class RestFulService {
         }
         return data;
     }
-    async getData(className:string, page:number, pageSize:number, where:string, includeRequest?:string){
+    //make it similar to parse object
+    preProcessJson(data:any){        
+        let keys = Object.keys(data);
+        if(!keys || keys.length<=0)return;
+
+        for(let key of keys){
+            if (data[key].__type && data[key].__type==="Pointer"){
+                data["_p_"+key]=data[key].className+"$"+data[key].objectId;
+                delete(data[key]);
+            }
+            if(data[key] && data[key]!=null && typeof(data[key])==="object") this.preProcessJson(data[key]);
+        }
+    }
+    sanitizeQuery(data:any){        
+        let keys = Object.keys(data);
+        if(!keys || keys.length<=0)return;
+
+        for(let key of keys){
+            if (key=="objectId"){
+                data["_id"]=data[key];
+                delete(data[key]);
+            }else
+            if (key=="createdAt"){
+                data["_created_at"]=data[key];
+                delete(data[key]);
+            }else
+            if (key=="updatedAt"){
+                data["_updated_at"]=data[key];
+                delete(data[key]);
+            }
+            if(data[key] && data[key]!=null && typeof(data[key])==="object") this.sanitizeQuery(data[key]);
+        }
+    }
+    //make it similar to parse object
+    postProcessJson(data:any){
+        let pointer="_p_";
+        let keys = Object.keys(data);
         
+        data.objectId = data._id;
+        data.createdAt = data._created_at;
+        data.updatedAt = data._updated_at;        
+        delete(data._id);
+        delete(data._created_at);
+        delete(data._updated_at);
+
+        for(let key of keys){
+            if(key.indexOf(pointer)>-1){
+                let newKey=key.substring(pointer.length,key.length);
+                let link = data[key].split('$');                     
+                data[newKey]={"__type": "Pointer", "className": link[0], "objectId": link[1]};
+                delete(data[key]);
+            }
+            if(data[key] && data[key]!=null && typeof(data[key])==="object") this.postProcessJson(data[key]);
+        }
+    }
+    async getData(className:string, page:number, pageSize:number, where:string, includeRequest?:string){        
         let data = await this.db.collection(className).findAsCursor(where).skip((page-1)*pageSize).limit(pageSize).toArray();
-        
+        for(let item of data){
+            this.postProcessJson(item);            
+        }
+
         if(!includeRequest)return data;
 
         return await this.fetchInclude(includeRequest, data);
