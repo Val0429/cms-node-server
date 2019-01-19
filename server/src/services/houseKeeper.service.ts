747096fd62a62d810790241ea7ad53ac5009dc61
Mohammad Firdaus Ardiansyah <mfirdaus2007@gmail.com>
2019-01-19 15:45:32 +0800

    Add feature 9401 delete log events schedule

---
+++ server/src/services/houseKeeper.service.ts
@@ -0,0 +1,44 @@
+import * as fs from 'fs';
+import * as util from 'util';
+import { ConfigHelper, LogHelper } from '../helpers';
+
+export class HouseKeeperService {
+    config = ConfigHelper.instance;
+    mongoist =require('mongoist');
+    logHelper = LogHelper.instance;
+
+    static get instance() {
+        return this._instance || (this._instance = new this());
+    }
+    private db:any;
+    private static _instance: HouseKeeperService;
+
+    constructor() {     
+        this.db = this.mongoist(`${this.config.parseConfig.DATABASE_URI}`); 
+        this.days = this.config.externalConfig.houseKeeper.keepDays || 90;        
+        //run check every hour
+        setInterval(this.startTask, 60 * 60 * 1000);
+        //this.startTask();
+    }
+    
+    days :number;
+
+    writeLog(d) {        
+        this.logHelper.writeLog({ type: 'HouseKeeper', msg: new Date().toLocaleString('en-US', { hour12: false }) + ' ' + util.format(d) + '\n' });               
+    }
+
+    async startTask() {
+        try{
+            let baseline = new Date();            
+            baseline.setDate(baseline.getDate() - this.days);            
+            this.writeLog("Delete Event and SystemLog older than " + baseline.toISOString());  
+            const delEvents$ =  this.db.collection('Event').remove({ _created_at: { $lt: baseline } });            
+            const delSystemLogs$ = this.db.collection('SystemLog').remove({ _created_at: { $lt: baseline } }) ;
+            await Promise.all([delEvents$, delSystemLogs$]);
+        }catch(err){
+            this.writeLog(err)
+        }
+    }
+        
+
+}
--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -1,3 +1,4 @@
 export * from './device.service'
 export * from './core.service'
 export * from './restful.service'
+export * from './houseKeeper.service'
